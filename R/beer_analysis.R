888f354154558b2bf97dd6a51582bf2a02fb7b42
LuukHenk <Luukperdaems@hotmail.com>
2020-09-16 19:52:50 +0200

    updated R script; It now uses ggplot

--- R/beer_analysis.R
+++ R/beer_analysis.R
@@ -1,4 +1,26 @@
-beers_info <- read.csv('../dataset/beers.csv', header=TRUE)
-plot(beers_info$abv, beers_info$ibu)
-boxplot(beers_info$abv)
-boxplot(beers_info$ibu)
+### Script info {{{
+# @title       | Beer analysis
+# @description | Generates a plot that compares the beer percentage, 
+#              | beer bitterness and beer style
+# @input       | The beer dataset found at '../dataset/beers.csv'
+#              | (Obtained from https://www.kaggle.com/nickhould/craft-cans)
+#	@output      | graphPlot containing the bitterness units on the x-axis and the
+#              | alcohol percentage on the y-axis. |
+# @authors     | Martijn Melissen, Angela Pelissou Ayuso, and Luuk Perdaems
+# }}}
+
+### Main script {{{
+# Import ggplot
+library(ggplot2)
+
+# Load data for plotting
+crafts_beer_data <- read.csv('../dataset/beers.csv', header=TRUE)
+
+# Plot data with ggplot
+ggplot(crafts_beer_data, aes(x = ibu, y= abv)) + 
+  geom_jitter() +
+  geom_smooth(method = "lm") +
+  theme(legend.position="none") +
+  labs(x = "Bittering units", y = "Alcohol content by volume") +
+  ggtitle('The relation between alcohol percentage and bitterness in craftbeers')
+# }}}
